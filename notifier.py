6814d1877d4add6b5d2bd8c16c04f1d0f80806da
Pratik-sys <pithore.pratik@gmail.com>
2020-02-29 22:21:28 +0530

    check the COD staus
     Check the value of "cod" key is equal to
    "404" to check the city is found or not found
    
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- notifier.py
+++ notifier.py
@@ -1,7 +1,9 @@
-import requests, json 
+import requests, json
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 
 def weather(city):
     api_key = os.getenv("PROJECT_API_KEY")
@@ -13,7 +15,35 @@ def weather(city):
     response = requests.get(complete_url)
 
     retrieve = response.json()
-    with open('city.json', 'w') as json_file:
+    with open("city.json", "w") as json_file:
         json.dump(retrieve, json_file)
+
+    # this will check the stauts if it is "404" or not.
+    if retrieve["cod"] != "404":
+        collect = retrieve["main"]
+
+        # store the corresponding value
+        # All the vlaues that will reflect in termianl are in "kelvin"
+
+        current_temprature = collect["temp"]
+
+        current_Humidity = collect["humidity"]
+
+        current_pressure = collect["pressure"]
+
+        # We need the weather description
+        # we make the store the data in another variable.
+
+        data = retrieve["weather"]
+        # we will stor the key at zeroth index of data
+        weather_description = data[0]["description"]
+
+        print(
+            f"Temprature(in Kelvin) = {current_temprature} \n Humidity(in %) = {current_Humidity} \n Atmosphric_pressure = {current_pressure} \n Description = {weather_description}"
+        )
+
+    else:
+        print("City not found please refer Map")
+
 
 weather(input("Enter the city name:"))
