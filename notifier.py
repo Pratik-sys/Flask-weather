46ae609a7b0e550d75467a3ad1bc3afae46bcecf
Pratik-sys <pithore.pratik@gmail.com>
2020-02-29 17:49:35 +0530

    Save json
    - This will help in understanding the things better.
    
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- notifier.py
+++ notifier.py
@@ -13,6 +13,7 @@ def weather(city):
     response = requests.get(complete_url)
 
     retrieve = response.json()
+    with open('city.json', 'w') as json_file:
+        json.dump(retrieve, json_file)
 
-    print(retrieve)
 weather(input("Enter the city name:"))
