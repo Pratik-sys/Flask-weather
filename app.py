850657b456b86e89985335d61a869a4b3d668521
Pratik-sys <pithore.pratik@gmail.com>
2020-03-13 18:58:56 +0530

    import the request module and define the function
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, request
+
+import requests
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/", methods=["POST", "GET"])
+def weather():
+    if request.method == "POST":
+        city = request.form["city"]
+    else:
+        city = "nagpur"
+    response = requests.get(
+        "http://api.openweathermap.org/data/2.5/weather?q="
+        + city
+        + "#"
+        + "&units=metric"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
